a841fe2ec7a1e624f827b265d2e53c21486c843d
Nick Lu <nicklu717@gmail.com>
2025-09-20 08:03:27 +0800

    fix: use .local/bin as deploy path

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 swift build -c release
-mkdir $HOME/bin
-cp .build/release/swift-module-package-manager-cli $HOME/bin/swift-module-package
-$SHELL
+bin_path="${HOME}/.local/bin"
+mkdir -p ${bin_path}
+cp .build/release/swift-module-package-manager-cli ${bin_path}/swift-module-package
+${SHELL}
